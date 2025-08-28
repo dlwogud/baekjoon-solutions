c75ba4b585efdbb24fd6d04afbcd71ecd4113875
dlwogud <89535380+dlwogud@users.noreply.github.com>
2025-08-28 16:02:21 +0900

    Add files via upload

---
+++ 백준 10815.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+x=[]
+s=int(input())
+a=set(map(int,input().split()))
+
+n=int(input())
+b=list(map(int,input().split()))
+
+for i2 in range(len(b)):
+    if b[i2] in a:
+        x.append(1)
+    else :
+        x.append(0)
+
+print(*x)
